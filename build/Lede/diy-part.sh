d04a38b773d93fecbc7a5baf9a106bf52cba7200
xiao-dd <59915536+xiao-dd@users.noreply.github.com>
2023-01-23 17:46:29 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,7 +4,7 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-export luci-app-nlbwmon
+export CONFIG_PACKAGE_luci-app-nlbwmon=y
 # 后台IP设置
 export Ipv4_ipaddr="192.168.98.2"            # 修改openwrt后台地址(填0为关闭)
 #export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
